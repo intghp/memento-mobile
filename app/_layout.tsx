e30e025599f26189b4668d66aac1971bc8d91b25
intghp <gustavohlucenapaiva1@gmail.com>
2026-08-26 14:41:49 -0300

    feat(frontend): inicialização na SplashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,37 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { initDB } from '../src/database/db';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+// Impede que a tela de carregamento (Splash Screen) suma sozinha
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [isDbReady, setDbReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // 1. O App liga e manda criar/conectar o Banco de Dados
+        await initDB();
+      } catch (error) {
+        console.error("Erro fatal ao carregar o app:", error);
+      } finally {
+        // 2. Quando o banco estiver pronto, o app é liberado
+        setDbReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // Enquanto o banco não estiver pronto, não desenha nenhuma tela por baixo
+  if (!isDbReady) {
+    return null;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
